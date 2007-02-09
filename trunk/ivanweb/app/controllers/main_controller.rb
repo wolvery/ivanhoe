d3d3080b54b0137cd14795acabfe9a062542169e
nicklaiacona <nicklaiacona@c161d011-2726-0410-8d5a-8f576c7000fa>
2007-02-09 16:00:52 +0000

    Made necessary changes to redirect top level menu items to pages served by wordpress.

--- trunk/ivanweb/app/controllers/main_controller.rb
+++
@@ -1,5 +0,0 @@
-class MainController < ApplicationController
-
-  layout "main_frame"
-
-end
--- trunk/ivanweb/config/routes.rb
+++ trunk/ivanweb/config/routes.rb
@@ -15,7 +15,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed by hooking up '' 
   # -- just remember to delete public/index.html.
-  map.connect '', :controller => "main", :action => "home"
+  #map.connect '', :controller => "main", :action => "home"
 
   # Allow downloading Web Service WSDL as a file with an extension
   # instead of a file named 'wsdl'
