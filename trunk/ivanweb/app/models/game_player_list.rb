30bbe21ef845dc61a0d3acad71d902cd0f97c2af
nicklaiacona <nicklaiacona@c161d011-2726-0410-8d5a-8f576c7000fa>
2007-03-13 19:07:54 +0000

    Games are now only displayed in "My Games" if I have actually made a move in the games.

--- trunk/ivanweb/app/models/game_player_list.rb
+++ trunk/ivanweb/app/models/game_player_list.rb
@@ -2,7 +2,7 @@ class GamePlayerList < ActiveRecord::Base
   set_table_name "player_game_role"   
   
   def self.get_players( game_id )
-    players_and_roles = find(:all, :conditions => "fk_game_id = #{game_id}" )
+    players_and_roles = find(:all, :conditions => [ "fk_game_id = ?", game_id ] )
     raw_players = []
     players_and_roles.each do |list_row|
       raw_players << Player.find(list_row.fk_player_id)
@@ -23,9 +23,15 @@ class GamePlayerList < ActiveRecord::Base
   end
   
   def self.get_games( player )
-   if not player then return nil end   
-   game_ids = find(:all, :conditions => "fk_player_id = #{player.id}")   
-   
+   return nil if not player
+      
+   game_ids = find(:all, :conditions => [ "fk_player_id = ?", player.id ] )   
+
+   # only get games which have this role participating in play
+   game_ids = game_ids.select { |game_id|
+    Move.find( :first, :conditions => ["fk_role_id = ?",game_id.fk_role_id ] ) != nil  
+   }
+
    games = []
    game_ids.each do |game_id|
     begin
@@ -40,7 +46,7 @@ class GamePlayerList < ActiveRecord::Base
   end
   
   def self.get_roles( game_id )
-    players_and_roles = find(:all, :conditions => "fk_game_id = #{game_id}")
+    players_and_roles = find(:all, :conditions => [ "fk_game_id = ?", game_id ] )
 
     roles = []
     players_and_roles.each do |list_row|
