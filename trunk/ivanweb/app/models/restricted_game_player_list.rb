77b3ca61fdbb7109bfff632e15714f1cf8a85664
nicklaiacona <nicklaiacona@c161d011-2726-0410-8d5a-8f576c7000fa>
2007-04-10 13:21:19 +0000

    Game creators can now edit the guest list for a restricted game during game creation or anypoint afterward.

--- trunk/ivanweb/app/models/restricted_game_player_list.rb
+++ trunk/ivanweb/app/models/restricted_game_player_list.rb
@@ -1,5 +1,18 @@
+require 'JSON'
+
 class RestrictedGamePlayerList < ActiveRecord::Base
   set_table_name "player_game"   
   
-  
+  def self.get_players( game_id )
+    game_player_list = find( :all, :conditions => ["fk_game_id = ?", game_id ])
+    players = []
+    for game_player in game_player_list
+      begin
+        players << Player.find(game_player.fk_player_id)
+      rescue
+      end
+    end
+    players
+  end
+   
 end
