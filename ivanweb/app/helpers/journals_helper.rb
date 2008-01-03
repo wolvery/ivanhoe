999bfb613a5d951236218854de08f0084a22c97d
nicklaiacona <nicklaiacona@c161d011-2726-0410-8d5a-8f576c7000fa>
2008-01-03 16:01:27 +0000

    Another potential fix for session problem.

--- ivanweb/app/helpers/journals_helper.rb
+++ ivanweb/app/helpers/journals_helper.rb
@@ -1,2 +1,24 @@
 module JournalsHelper
+
+def logged_in?
+    @current_user
+  end
+  
+  def current_user
+    @current_user
+  end
+
+  def admin?
+    ( @current_user and @current_user.admin )
+  end
+  
+  def can_create_game?
+    ( @current_user and @current_user.new_game_permission )
+  end
+  
+  # a user can edit a particular game if they are the creator of the game or an admin
+  def can_edit_game?(game_id)
+    ( admin? or ( @current_user and @current_user.can_edit_game?(game_id) ) )
+  end
+
 end
