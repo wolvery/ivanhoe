2de71d570932ee70b81018063fe9c2eac4cd0853
nicklaiacona <nicklaiacona@c161d011-2726-0410-8d5a-8f576c7000fa>
2008-01-03 16:37:08 +0000

    Another potential fix for session problem.

--- ivanweb/app/helpers/application_helper.rb
+++ ivanweb/app/helpers/application_helper.rb
@@ -1,3 +1,25 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def logged_in?
+    not @current_user.nil?
+  end
+  
+  def current_user
+    @current_user
+  end
+
+  def admin?
+    ( logged_in? and @current_user.admin )
+  end
+  
+  def can_create_game?
+    ( logged_in? and @current_user.new_game_permission )
+  end
+  
+  # a user can edit a particular game if they are the creator of the game or an admin
+  def can_edit_game?(game_id)
+    ( admin? or ( logged_in? and @current_user.can_edit_game?(game_id) ) )
+  end
+
 end
