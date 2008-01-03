acee9487f4f7bfe0be3acae436bb7f3cf4aa60bc
nicklaiacona <nicklaiacona@c161d011-2726-0410-8d5a-8f576c7000fa>
2008-01-03 16:37:08 +0000

    Another potential fix for session problem.

--- trunk/ivanweb/app/helpers/games_helper.rb
+++ trunk/ivanweb/app/helpers/games_helper.rb
@@ -1,7 +1,7 @@
 module GamesHelper
   
   def logged_in?
-    @current_user
+    not @current_user.nil?
   end
   
   def current_user
@@ -9,16 +9,16 @@ module GamesHelper
   end
 
   def admin?
-    ( @current_user and @current_user.admin )
+    ( logged_in? and @current_user.admin )
   end
   
   def can_create_game?
-    ( @current_user and @current_user.new_game_permission )
+    ( logged_in? and @current_user.new_game_permission )
   end
   
   # a user can edit a particular game if they are the creator of the game or an admin
   def can_edit_game?(game_id)
-    ( admin? or ( @current_user and @current_user.can_edit_game?(game_id) ) )
+    ( admin? or ( logged_in? and @current_user.can_edit_game?(game_id) ) )
   end
 
 end
